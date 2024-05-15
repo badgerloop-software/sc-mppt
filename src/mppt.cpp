daa1b20685370edc8028523594cfd4e943a293c6
bquan0 <powerbowen@gmail.com>
2024-05-14 22:16:36 -0500

    gradient descent implementation of constant current

--- src/mppt.cpp
+++ src/mppt.cpp
@@ -8,19 +8,34 @@ void mpptUpdate() {
     // Tracks last power
     static float oldPower = 0.0;
     static float stepSize = INIT_VOLT_STEP;
+    static float oldCurr = 0.0;
+    static float stepSize_C = INIT_VOLT_STEP;
 
-    // Constant current mode. Try to match BMS provided charge current limit via PID loop
+    // Constant current mode. Try to match BMS provided charge current limit via gradient descent
     if (chargeMode == ChargeMode::CONST_CURR) {
         // Compute output current for feedback
         float totalInputPower = 0;
         for (int i = 0; i < NUM_ARRAYS; i++) {
             totalInputPower += arrayData[i].curPower;
         }
-        float outputCurr = totalInputPower / battVolt;
-        
+        float newCurr = totalInputPower / battVolt;
+
+        // newCurr is farther from Limit than oldCurr
+        // so, we should step in opposite direction
+        if (abs(newCurr - packChargeCurrentLimit) < abs(oldCurr - packChargeCurrentLimit)) {
+            stepSize_C *= -1.0;
+        }
+
+        // step NOW voltage in desired direction
+        float targetVoltage_C = arrayData[0].voltage + stepSize_C;
+        if (targetVoltage_C <= 0) targetVoltage_C = 0.01;
+        setVoltOut(targetVoltage_C);
+
         // Get new target voltage from PID and update output
-        float targetVoltage = updateCurrentOut(outputCurr);
-        setVoltOut(targetVoltage);
+        // float targetVoltage = updateCurrentOut(outputCurr);
+        // setVoltOut(targetVoltage);
+
+        oldCurr = newCurr;
         return;
     }
 
